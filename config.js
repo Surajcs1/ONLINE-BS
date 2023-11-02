1645cce3028c631e3fd7589221d9d0f388d60b83
Surajcs1 <143376744+Surajcs1@users.noreply.github.com>
2023-11-02 15:12:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://surajcs9307:omwwwLFRoo2o8Fju@cluster0.lmjyrtb.mongodb.net/?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
